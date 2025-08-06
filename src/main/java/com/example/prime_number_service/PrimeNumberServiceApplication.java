3bab8501abc92bdb60c4d3571dd63b36dfa944e6
Amandeep Bassi <asbassi@outlook.com>
2025-08-06 17:25:35 +0100

    initial commit

---
+++ src/main/java/com/example/prime_number_service/PrimeNumberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.prime_number_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeNumberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeNumberServiceApplication.class, args);
+	}
+
+}
